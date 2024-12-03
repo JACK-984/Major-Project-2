8a99b937bc2b587eb298824fdcce3f1f5e479706
ICB999 <bensey873@gmail.com>
2024-12-03 23:23:50 +0700

    added google sign in without prisma and mongodb yet

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { signIn, useSession } from "next-auth/react";
 import {
   Card,
   CardContent,
@@ -9,8 +11,10 @@ import {
 } from "@/components/ui/card";
 import { Icons } from "@/components/ui/icons";
 import Link from "next/link";
+import { log } from "console";
 
 export default function LoginPage() {
+  const { data: session } = useSession();
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4 py-12 sm:px-6 lg:px-8">
       <Card className="w-full max-w-md">
@@ -24,9 +28,13 @@ export default function LoginPage() {
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <Button className="w-full" variant="outline">
+          <Button className="w-full" variant="outline" onClick={() => signIn("google")}>
             <Icons.google className="mr-2 h-4 w-4" />
             Sign in with Google
+          </Button>
+          <Button className="w-full" variant="outline" onClick={() => console.log(session)}>
+            <Icons.google className="mr-2 h-4 w-4" />
+            LOG OUT SESSION
           </Button>
         </CardContent>
         <CardFooter>
