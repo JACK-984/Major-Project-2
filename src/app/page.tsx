909f5b098c9dccbb6518f8542dd4772b8ef1096b
ICB999 <bensey873@gmail.com>
2025-01-04 18:08:14 +0700

    connected authjs with mongodb via prisma accelerate [TODO: work on importing GMAIL API & mailing data]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
@@ -9,6 +9,7 @@ export default function Home() {
     <div>
       {session?.user && <p>Logged in</p>}
       <Button onClick={() => signIn("google")}>test sign in</Button>
+      <Button onClick={() => signOut()}>test sign out</Button>
       <Button onClick={() => console.log(session?.user)}>Log out session</Button>
     </div>
   );
